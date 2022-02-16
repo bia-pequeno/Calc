708394f4de44fb50c07fa5945076812cb3bad6a5
bia-pequeno <bia.2017pequeno@gmail.com>
2022-02-16 18:20:13 -0300

    concluido

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class calc extends Component{
+
+  state = {
+    result: 0,
+    num1: null,
+    num2: null
+  }
+
+  handleChange = (ev) => {
+    this.setState({
+      num1: ev.target.value
+    })
+  }
+  handleChange2 = (ev) => {
+    this.setState({
+      num2: ev.target.value
+    })
+  }
+
+  minus = () => {
+    let {num1,num2} = this.state
+    this.setState({
+      result: num1 - num2
+
+    })
+  }
+  multiply = () => {
+    let {num1,num2} = this.state
+    this.setState({
+      result: num1 * num2
+    })
+  }
+  divide = () => {
+    let {num1,num2} = this.state
+    this.setState({
+      result: num1 / num2 
+    })
+  }
+  add = () => {
+    let {num1,num2} = this.state
+    this.setState({
+      result: Number(num1) + Number(num2) 
+    })
+  }
+  
+  render(){
+    return(
+      <div className='container'>
+        {console.log(this.state.num1, this.state.num2)}
+        <h1>React Calc App</h1>
+        <h2>{this.state.result}</h2>
+        <div>
+          <input onChange={this.handleChange} type='Number'/>
+          <input onChange={this.handleChange2} type='Number'/>
+        </div>
+        <div>
+        <button onClick={this.minus}>-</button>
+          <button onClick={this.multiply}>*</button>
+          <button onClick={this.divide}>/</button>
+          <button onClick={this.add}>+</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default calc
