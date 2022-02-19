9b7deb61af71ac71f4c93a9ae75fa136a11f439c
bia-pequeno <bia.2017pequeno@gmail.com>
2022-02-19 16:01:11 -0300

    concluido

--- src/App.js
+++ src/App.js
@@ -11,56 +11,95 @@ class calc extends Component{
 
   handleChange = (ev) => {
     this.setState({
-      num1: ev.target.value
+      num1: Number(ev.target.value) 
     })
   }
   handleChange2 = (ev) => {
     this.setState({
-      num2: ev.target.value
+      num2: Number(ev.target.value)
     })
   }
 
   minus = () => {
     let {num1,num2} = this.state
-    this.setState({
-      result: num1 - num2
-
-    })
+    if(num1 && num2 !== null){
+      this.setState({
+        result: num1 - num2
+      })
+    }else{
+      this.setState({
+        result: "Nenhum valor foi digitado"
+      })
+    }
   }
   multiply = () => {
     let {num1,num2} = this.state
-    this.setState({
-      result: num1 * num2
-    })
+    if(num1 && num2 !== null){
+      this.setState({
+        result: num1 * num2
+      })
+    }else{
+      this.setState({
+        result: "Nenhum valor foi digitado"
+      })
+    }
   }
   divide = () => {
     let {num1,num2} = this.state
-    this.setState({
-      result: num1 / num2 
-    })
+    if(num1 && num2 !== null){
+      this.setState({
+        result: num1 / num2
+      })
+    }else{
+      this.setState({
+        result: "Nenhum valor foi digitado"
+      })
+    }
   }
   add = () => {
     let {num1,num2} = this.state
-    this.setState({
-      result: Number(num1) + Number(num2) 
-    })
+    if(num1 && num2 !== null){
+      this.setState({
+        result: num1 + num2
+      })
+    }else{
+      this.setState({
+        result: "Nenhum valor foi digitado"
+      })
+    }
+  }
+  clear = ()=>{
+    let {num1, num2} = this.state
+    if(num1 && num2 !== " "){
+      this.setState({
+        result: null,
+        num1: 0,
+        num2: 0
+      })
+    }else{
+      this.setState({
+        result: "Nenhum valor foi digitado"
+      })
+    }
   }
   
   render(){
+    let {minus, add, divide, multiply, clear, handleChange, handleChange2} = this
+    let {num1, num2, result,} = this.state
     return(
       <div className='container'>
-        {console.log(this.state.num1, this.state.num2)}
         <h1>React Calc App</h1>
-        <h2>{this.state.result}</h2>
+        <h2>{result}</h2>
         <div>
-          <input onChange={this.handleChange} type='Number'/>
-          <input onChange={this.handleChange2} type='Number'/>
+          <input onChange={handleChange} type='Number' value={num1}/>
+          <input onChange={handleChange2} type='Number' value={num2}/>
         </div>
         <div>
-        <button onClick={this.minus}>-</button>
-          <button onClick={this.multiply}>*</button>
-          <button onClick={this.divide}>/</button>
-          <button onClick={this.add}>+</button>
+        <button onClick={minus}>-</button>
+          <button onClick={multiply}>*</button>
+          <button onClick={divide}>/</button>
+          <button onClick={add}>+</button>
+          <button onClick={clear}>Clear</button>
         </div>
       </div>
     )
